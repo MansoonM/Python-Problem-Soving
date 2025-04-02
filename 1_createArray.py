d94c4b369203c83681e6e08b4f53f0f7f85362ee
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-02 06:36:53 +0530

    Add files via upload

---
+++ 1_createArray.py
@@ -0,0 +1,12 @@
+# Numeric value store
+
+arrayForInt=[23,43,21,52,41];
+for i in arrayForInt:
+    print(i)
+  
+
+
+# String  value store
+arrayForString=["books","copies","pens","pencils","colors"]
+for x in arrayForString:
+    print(x)
