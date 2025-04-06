c0791420ed3ef10ea3f55efca4a9f270d98e50a7
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-06 08:57:18 +0530

    Add files via upload

---
+++ 2_find_length_of_string.py
@@ -0,0 +1,10 @@
+# String User Input
+string= str(input("Enter your thoughts: "))
+print("Your thought is : " + string)
+
+# length of string (way 1)
+length_of_String= len(string)
+print("The length of string is ", length_of_String)
+
+# length of string (way 2)
+print("The length of string is ", len(string))
