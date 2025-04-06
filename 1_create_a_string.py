477f902d99be45cb126684ae981dc57dd1cac777
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-06 08:55:59 +0530

    Add files via upload

---
+++ 1_create_a_string.py
@@ -0,0 +1,10 @@
+string_1= "Python"
+string_2= "This is Python"
+
+
+print(string_1)
+print(string_2)
+
+# String User Input
+string= str(input("Enter your thoughts: "))
+print("Your thought is : " , string)
