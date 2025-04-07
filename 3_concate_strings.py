5ea06a1b40f38f76f9c138c92bb0227a801b1ffd
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-07 18:35:51 +0530

    Add files via upload

---
+++ 3_concate_strings.py
@@ -0,0 +1,12 @@
+#way 1
+string1="I love"
+string2="me"
+print(string1,string2)
+
+
+#way 2
+string_1= str(input("Enter first string: "))
+string_2= str(input("Enter second string: "))
+string_3= str(input("Enter third string: "))
+print("After concatination of three string: " , "'",string_1, string_2, string_3,"'")
+
