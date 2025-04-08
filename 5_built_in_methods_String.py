6afed241cdf80d7cc76592bee61d4c725b2c24b9
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-08 07:53:49 +0530

    Add files via upload

---
+++ 5_built_in_methods_String.py
@@ -0,0 +1,9 @@
+x= str(input("Any String: "))
+
+print("Uppercase: " , x.upper())
+print("Lowercase: " , x.lower())
+
+# print("No Space Between Words: " , x.strip())
+
+print("Replace ", x.replace(x[0],"B"))
+print("Replace ", x.replace(x[-1],"B")) #last index element "e"= "B"
