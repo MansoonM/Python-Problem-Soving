b0f9d204ec04c2306f66a6856febcd988f653bd0
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-04-08 07:53:33 +0530

    Add files via upload

---
+++ 4_accessElementInString.py
@@ -0,0 +1,12 @@
+string="This is my favorite book"
+print(string[9])
+
+#NOTE: Strings are array in python
+
+#slicing (start,stop,step)
+print(string[0:10:1])
+print(string[0:10:2]) #step 2
+print(string[:])     # print all
+print(string[-1]) # access last element
+print(string[-5:-3]) 
+print(string[::-1]) #everything gets reversed like... is=si, this=siht
